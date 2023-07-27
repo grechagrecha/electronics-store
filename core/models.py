12e0f892bcca501930110c5cc168b827a1886ba7
grecha_grecha <ruspolak69@gmail.com>
2023-07-27 08:20:17 +0300

    final master init

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 class Item(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     price = models.IntegerField()
     units_sold = models.BigIntegerField()
     units_in_stock = models.BigIntegerField()
@@ -19,8 +19,3 @@ class Item(models.Model):
         return reverse('core:product', kwargs={
             'slug': self.slug
         })
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = f'{slugify(self.name)}-{slugify(self.id)}'
-            super(Item, self).save(*args, **kwargs)
--- store/settings/__init__.py
+++ store/settings/__init__.py
@@ -3,4 +3,4 @@ from .base import *
 try:
     from .local import *
 except ImportError:
-    print('Can\'t fing module settings.local')
+    print('Can\'t fing module settings.local. Use settings.local.skeleton as a template to create it.')
