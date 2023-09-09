eb4ffe681af79978b93abf735f30f181043dead9
grecha_grecha <ruspolak69@gmail.com>
2023-09-09 15:42:54 +0300

    add dotenv and hide secret key

--- store/settings/__init__.py
+++ store/settings/__init__.py
@@ -1,6 +1,1 @@
-from .base import *
-
-try:
-    from .local import *
-except ImportError:
-    print('Can\'t fing module settings.local. Use settings.local.skeleton as a template to create it.')
+from .settings import *
