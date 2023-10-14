5699a573e2e5299b4c8e946a0b6df0d2e299ca57
grecha_grecha <ruspolak69@gmail.com>
2023-10-14 16:25:19 +0300

    hotfix 3

--- store/settings/settings.py
+++ store/settings/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-DEBUG = str(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = [
     'grechagrecha.pythonanywhere.com',
